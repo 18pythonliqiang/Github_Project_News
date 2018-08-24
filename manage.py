f3ff7aa1cb3d885ef4250aadf5a0580fd5217f13
manage_liqiang <1209864111@qq.com>
2018-08-24 19:53:03 +0800

    mysql配置

--- manage.py
+++ manage.py
@@ -1,13 +1,35 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+# 创建项目配置类
+
+class Config(object):
+
+    # 开启debug
+    DEBUG = True
+
+    #mysql数据库配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/informtion"
+
+    # 开启数据库跟踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# 创建app对象
 app = Flask(__name__)
 
-@app.route("/index")
+# 注册配置信息到app中
+app.config.from_object(Config)
+
+# 创建数据库对象
+db = SQLAlchemy(app)
+
+@app.route("/")
 
 def index():
 
-    return "index"
+    return "心境资讯"
 
 if __name__ == '__main__':
 
-    app.run()
+    app.run(debug=True)
