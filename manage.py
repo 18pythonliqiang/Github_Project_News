613a91820176a5c6564e27d49f7addbe60b4d021
manage_liqiang <1209864111@qq.com>
2018-08-24 21:43:16 +0800

    抽离项目配置信息类，创建config.py文件配置信息，通过继承config类提供不同的开发环境，开发者模式与生产模式

--- manage.py
+++ manage.py
@@ -8,59 +8,29 @@ from flask_wtf import CSRFProtect
 
 from flask_session import Session
 
-# 创建项目配置类
+from flask_script import Manager
 
-class Config(object):
-
-    # 开启debug
-    DEBUG = True
-
-    #mysql数据库配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/informtion"
-
-    # 开启数据库跟踪
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    #redis数据库的配置信息
-    REDIS_HOST = "127.0.0.1"
-
-    REDIS_PORT = 6379
-
-    REDIS_NUM = 7
-
-    #session配置
-    SECRET_KEY = "dasdasfewffdasdas"
-
-    # 调整session存储到redis的配置信息
-    # 设置储存session的数据库类型
-    SESSION_TYPE = "redis" # 指定 session 保存到 redis 中
-
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,
-
-                                port=REDIS_PORT,
-
-                                db=REDIS_NUM) # 使用 redis 的实例
-
-    SESSION_USE_SIGNER = True # 让 cookie 中的 session_id 被加密签名处理
-
-    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期，单位是秒
-
+from config import config_dict
 
 # 创建app对象
 app = Flask(__name__)
 
 # 注册配置信息到app中
-app.config.from_object(Config)
+
+# config_dict["development"]--->DevelopmentConfig
+config_class = config_dict["development"]
+
+app.config.from_object(config_class)
 
 # 创建数据库对象
 db = SQLAlchemy(app)
 
 # 创建redis数据库对象
-redis_store = StrictRedis(host=Config.REDIS_HOST,
+redis_store = StrictRedis(host=config_class.REDIS_HOST,
 
-                          port=Config.REDIS_PORT,
+                          port=config_class.REDIS_PORT,
 
-                          db=Config.REDIS_NUM,)
+                          db=config_class.REDIS_NUM,)
 
 # 开启flask后端csrf验证保护机制
 
@@ -72,6 +42,10 @@ csrf = CSRFProtect(app)
 
 Session(app)
 
+# 创建manager管理类
+
+manager = Manager(app)
+
 @app.route("/")
 
 def index():
@@ -82,4 +56,4 @@ def index():
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    manager.run()
