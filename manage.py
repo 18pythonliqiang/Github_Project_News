89ae10a522a1cc442f1077b938aabc8dcd22fdab
manage_liqiang <1209864111@qq.com>
2018-08-24 21:14:59 +0800

    用flask-session第三方库调整flask session储存到redis数据库中

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask,session
 
 from flask_sqlalchemy import SQLAlchemy
 
 from redis import StrictRedis
+
+from flask_wtf import CSRFProtect
+
+from flask_session import Session
 
 # 创建项目配置类
 
@@ -24,11 +28,22 @@ class Config(object):
 
     REDIS_NUM = 7
 
+    #session配置
+    SECRET_KEY = "dasdasfewffdasdas"
 
+    # 调整session存储到redis的配置信息
+    # 设置储存session的数据库类型
+    SESSION_TYPE = "redis" # 指定 session 保存到 redis 中
 
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,
 
+                                port=REDIS_PORT,
 
+                                db=REDIS_NUM) # 使用 redis 的实例
 
+    SESSION_USE_SIGNER = True # 让 cookie 中的 session_id 被加密签名处理
+
+    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期，单位是秒
 
 
 # 创建app对象
@@ -45,15 +60,25 @@ redis_store = StrictRedis(host=Config.REDIS_HOST,
 
                           port=Config.REDIS_PORT,
 
-                          db=Config.REDIS_NUM,
+                          db=Config.REDIS_NUM,)
 
-                          decode_responses=True)
+# 开启flask后端csrf验证保护机制
+
+csrf = CSRFProtect(app)
+
+# 借助第三方session类去调整flask中的session存储位置
+
+# flask_session的配置信息
+
+Session(app)
 
 @app.route("/")
 
 def index():
+    # 之前session数据存储在服务器上
+    session["name"] = "liqiang"
 
-    return "心境资讯"
+    return "aaaa"
 
 if __name__ == '__main__':
 
